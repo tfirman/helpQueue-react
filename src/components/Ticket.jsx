e035f4fb2b2eaceab148fe05676173ba072bd442
Tim Firman <tkfirman@gmail.com>
2018-04-18 08:10:33 -0700

    follows lesson

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types';
 function Ticket(props){
   return (
     <div>
-      <style global jsx>{`
+      <style jsx>{`
         div {
-          background-color: red;
+          background-color: bisque;
         }
       `}</style>
       <h3>{props.location} - {props.names}</h3>
