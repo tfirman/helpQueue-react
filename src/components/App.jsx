2bb99f250c2c5d71b6c735c6443cbed163211139
Tim Firman <tkfirman@gmail.com>
2018-04-24 10:37:51 -0700

    start on form input

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import Header from './Header';
 import TicketList from './TicketList';
 import MyStyledComponent from './MyStyledComponent';
+import NewTicketControl from './NewTicketControl';
+import Error404 from './Error404';
 import { Switch, Route } from 'react-router-dom';
-import NewTicketForm from './NewTicketForm';
 
 function App(){
   return (
@@ -11,8 +12,9 @@ function App(){
       <Header/>
       <Switch>
         <Route exact path='/' component={TicketList} />
-        <Route path='/newticket' component={NewTicketForm} />
+        <Route path='/newticket' component={NewTicketControl} />
         <Route path='/msc' component={MyStyledComponent} />
+        <Route component={Error404} />
       </Switch>
     </div>
   );
