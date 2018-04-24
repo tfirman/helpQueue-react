1a34999c9295d57a4461f1030d45309ae99db185
Tim Firman <tkfirman@gmail.com>
2018-04-24 14:32:50 -0700

    lintfix

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -6,7 +6,7 @@ function TicketList(props){
   return (
     <div>
       <hr/>
-      {props.ticketList.map((ticket, index) =>
+      {props.ticketList.map((ticket) =>
         <Ticket names={ticket.names}
           location={ticket.location}
           issue={ticket.issue}
