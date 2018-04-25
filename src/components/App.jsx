2e6b8a6ae2ea449dd276191f865edf68f1bc29ff
Tim Firman <tkfirman@gmail.com>
2018-04-25 12:50:17 -0700

    adds timer functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ import MyStyledComponent from './MyStyledComponent';
 import NewTicketControl from './NewTicketControl';
 import Error404 from './Error404';
 import { Switch, Route } from 'react-router-dom';
+import Moment from 'moment';
 
 class App extends React.Component {
 
@@ -16,8 +17,29 @@ class App extends React.Component {
     this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
   }
 
+  componentDidMount() {
+    this.waitTimeUpdateTimer = setInterval(() =>
+      this.updateTicketElapsedWaitTime(),
+    60000
+    );
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.waitTimeUpdateTimer);
+  }
+
+  updateTicketElapsedWaitTime() {
+     let newMasterTicketList = this.state.masterTicketList.slice();
+     newMasterTicketList.forEach((ticket) =>
+       ticket.formattedWaitTime = (ticket.timeOpen).fromNow(true)
+     );
+     this.setState({masterTicketList: newMasterTicketList});
+   }
+
+
   handleAddingNewTicketToList(newTicket){
     var newMasterTicketList = this.state.masterTicketList.slice();
+    newTicket.formattedWaitTime = (newTicket.timeOpen).fromNow(true);
     newMasterTicketList.push(newTicket);
     this.setState({masterTicketList: newMasterTicketList});
   }
